72bc062df63f1cba4ec160cfe5b7b22f49ecc0ca
Poigrammer <atanas123atanasov@gmail.com>
2022-06-21 20:49:16 +0300

    first-commit

---
+++ KanbanizeCardTest/CardTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace KanbanizeCardTest
+{
+    public class CardTest
+    {
+        [Fact]
+        public void CreatingTest()
+        {
+            
+        }
+    }
+}
